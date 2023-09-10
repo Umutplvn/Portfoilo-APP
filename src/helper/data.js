8edc03ab3d6b64bde8be27efc177021887c6a4d3
umutplvn <umutpehlivan2078@gmail.com>
2023-09-10 07:35:03 +0100

    v3

---
+++ src/helper/data.js
@@ -0,0 +1,30 @@
+export const data =[
+    {   type:"FINANCIAL MANAGEMENT APPLICATION",
+        name:"Stock App",
+        desc:"Financial management app for tracking income, expenses, and budgeting efficiently.",
+        built:"Redux, JS, Vite and more",
+        imgLink:"../../public/helperImg/stockApp.png",
+        imgLink:"https://ibb.co/9ZdcRQ5",
+        link:"stockapp-umut.netlify.app",
+        id:1
+
+    },
+
+    {   type:"BLOG",
+    name:"Legere",
+    desc:"Interactive blogging platform for engaging content creators and readers.",
+    built:"React, Redux Toolkit, JS, and more",
+    imgLink:"../assets/helperImg/blogApp.png",
+    link:"legere.netlify.app/",
+    id:2
+},
+{   type:"E-COMMERCE APPLICATION",
+    name:"Shopping App",
+    desc:"Online store for shopping",
+    built:"React, JS, MUI, and more",
+    imgLink:"../assets/helperImg/blogApp.png",
+    link:"shopping-app-umut.netlify.app/",
+    id:3
+}
+
+]
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import "./Portfolio.css"
+import { Box } from '@mui/material';
+import Carousel from 'react-material-ui-carousel';
+import {data} from "../helper/data"
+
 const Portfolio = () => {
+
+
   return (
-    <div className="portfolio_main" id='portfolio'zIndex={0}>
-       
-        <div className='portfolio_box'>
 
 
-        </div>
-    </div>
+<div id="portfolio" className='portfolio_main'> 
+  <img src="../../public/blogApp.png" style={{width:"200px", height:"200px"}} alt="" />
+</div>
   )
 }
 
 export default Portfolio
+
+{/* <Carousel id="portfolio" className='portfolio_main'>
+{data.map((item)=>
+<Box key={item.id}>
+  <img src={item.imgLink}/>
+
+<h1 style={{color:"white"}}>{item.name}</h1>
+</Box>
+
+)}
+</Carousel> */}
